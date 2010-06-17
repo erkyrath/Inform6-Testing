c47da623b78db567b6682ee2b9db34a1e038e535
Andrew Plotkin <erkyrath@eblong.com>
2010-06-16 21:17:00 -0400

    Updated readme.txt and copyright dates to match official I631N release.

--- header.h
+++ header.h
@@ -30,7 +30,7 @@
 /*                                                                           */
 /* ------------------------------------------------------------------------- */
 
-#define RELEASE_DATE "16th November 2009"
+#define RELEASE_DATE "22nd April 2010"
 #define RELEASE_NUMBER 1631
 #define GLULX_RELEASE_NUMBER 38
 #define MODULE_VERSION_NUMBER 1
