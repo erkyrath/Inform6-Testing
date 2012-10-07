adac95842577802a1948f3efa122a9a55620b904
Andrew Plotkin <erkyrath@eblong.com>
2012-10-06 22:26:32 -0400

    1013: Stop using the fixed-pitch header bit in font on/off
    Use @set_font instead of @set_text_style. Sorry about that.

--- states.c
+++ states.c
@@ -919,15 +919,14 @@ static void parse_statement_z(int break_label, int continue_label)
                  }
 
                  if (version_number >= 5)
-                 {   /* Use the V5 @set_text_style opcode. This is
-                        equivalent to "style fixed" (for font off) or
-                        "style roman" (for font on). */
+                 {   /* Use the V5 @set_font opcode, setting font 4
+                        (for font off) or 1 (for font on). */
                      AO.type = SHORT_CONSTANT_OT; AO.marker = 0;
                      if (token_value == ON_MK)
-                         AO.value = 0;
+                         AO.value = 1;
                      else
-                         AO.value = 8;
-                     assemblez_1(set_text_style_zc, AO);
+                         AO.value = 4;
+                     assemblez_1_to(set_font_zc, AO, temp_var1);
                      break;
                  }
 
