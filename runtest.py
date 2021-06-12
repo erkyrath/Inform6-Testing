492a01a1096c2e573edf0930e6bbca8e90c2e000
Andrew Plotkin <zarf@ZarfLent.local>
2021-06-12 10:55:22 -0400

    More warnings.

--- runtest.py
+++ runtest.py
@@ -774,10 +774,10 @@ def run_modules_test():
 
 def run_warnings_test():
     res = compile('typewarningtest.inf')
-    res.is_ok(warnings=27)
+    res.is_ok(warnings=37)
     
     res = compile('typewarningtest.inf', glulx=True)
-    res.is_ok(warnings=29)
+    res.is_ok(warnings=39)
     
 
 def run_make_abbreviations_test():
