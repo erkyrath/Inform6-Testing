b2447c27392ec2e8f8ad2f61e8ba5a1d09cab402
Andrew Plotkin <zarf@ZarfLent.local>
2021-07-24 11:38:11 -0400

    Adjust tests affected by the Stub stackargs bug. (This only affects I7 source files.)

--- runtest.py
+++ runtest.py
@@ -459,13 +459,13 @@ def run_checksum_test():
     res.is_ok(md5='5feea90b2cf68a270d33795245008383')
 
     res = compile('i7-min-6G60.inf', glulx=True)
-    res.is_ok(md5='e9d3046de7a45028812aad9d5a132d32')
+    res.is_ok(md5='1a19f400c9cf5901148af7f7503cc745')
 
     res = compile('i7-min-6M62-z.inf', zversion=8)
     res.is_ok(md5='5d684cd1f5028c923ec16fe4761ed5c9')
 
     res = compile('i7-min-6M62-g.inf', glulx=True)
-    res.is_ok(md5='ccb67fdaf4a272dfd4b829cc4f2202f6')
+    res.is_ok(md5='522f498a76e2cdd8f27553f76c6445ac')
 
     res = compile('Advent.inf', includedir='i6lib-611')
     res.is_ok(md5='453977372e150037f9f3f93cdf847e35', warnings=0)
@@ -1370,7 +1370,7 @@ def run_omit_unused_routines():
 
     res = compile('i7-min-6G60.inf', memsettings={'OMIT_UNUSED_ROUTINES':1}, glulx=True)
     res.is_ok()
-    res.is_ok(md5='b4754e75f23e1d6bf922e0fa6f236177')
+    res.is_ok(md5='691e2c5d7c15f4b49fdcefdae6f28aae')
 
 
 test_catalog = [
