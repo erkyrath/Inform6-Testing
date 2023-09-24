63af23ddfc895a164a72675116cfe66cee1797fb
Andrew Plotkin <zarf@ZarfLent.local>
2023-09-23 23:52:18 -0400

    A couple more bads.

--- runtest.py
+++ runtest.py
@@ -1065,6 +1065,12 @@ def run_expressions_test():
     res = compile('unaryop_err_test.inf', define={'BAD_EXPR_11':None})
     res.is_error(errors=2)
     
+    res = compile('unaryop_err_test.inf', define={'BAD_EXPR_12':None})
+    res.is_error(errors=3)
+    
+    res = compile('unaryop_err_test.inf', define={'BAD_EXPR_13':None})
+    res.is_error(errors=2)
+    
 
 def run_debugflag_test():
     res = compile('no_debug_flag_test.inf')
